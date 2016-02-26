cc45091e832e7280b2b50ebc6a30e2b15e15cf05
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-02-26 14:38:10 -0600

    move docker nuke etc to dashes

--- recipes/default.rb
+++ recipes/default.rb
@@ -37,7 +37,7 @@ node_conf["containers"].each do |c|
   end
 end
 
-%w(docker_clean docker_nuke docker_names).each do |f|
+%w(docker-clean docker-nuke docker-names).each do |f|
   cookbook_file f do
     path "/usr/local/bin/#{f}"
     mode "0755"
